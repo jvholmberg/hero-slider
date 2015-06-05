6836ed6721168fe74c44704ab4b1f5e8407d6a38
jwholmberg <johan.holmberg123@hotmail.com>
2015-06-05 19:38:59 +0200

    buggfix
    fixed issue with picutures shining through during animation in
    fade-option. + some refactoring including constants which are now longer
    in the solution.

--- src/js/lib/hero-slider/js/hero.js
+++ src/js/lib/hero-slider/js/hero.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     *                                                     *
     ******************************************************/
 
-        sleepTimer: 8000,
+        sleepTimer: 4000,
         animationSpeed: 1500,
         contentHeight: 129,
         minHeight: 0,
@@ -18,7 +18,7 @@ $(document).ready(function () {
         filePath: 'src/js/lib/hero-slider/assets/',
         files: ['hero-road', 'hero-lake', 'hero-field'],
         fileType: '.jpg',
-        slideInFomSide: false,
+        slideInFomSide: true,
 
     /******************************************************
     *                                                     *
@@ -32,9 +32,9 @@ $(document).ready(function () {
         $elements: $('.hero-slide'),
         $overlay: null,
         $radio: null,
-        AUTO: 1,
         interval: null,
         current: 0,
+        previous: 0,
 
         init: function () {
             var $css =
@@ -81,6 +81,7 @@ $(document).ready(function () {
             xHero.$overlay.css('margin-top', -50);
             xHero.$list.css('width', xHero.slideInFomSide ? 100 * (xHero.files.length + 1) + '%' :  xHero.width + '%');
             xHero.$elements.css({
+                'display': xHero.slideInFomSide ? 'block' : 'none',
                 'position': xHero.slideInFomSide ? 'relative' : 'absolute',
                 'float': xHero.slideInFomSide ? 'left' : 'none',
                 'min-height': xHero.minHeight,
@@ -93,6 +94,7 @@ $(document).ready(function () {
                     'z-index': xHero.files.length - index
                 });
             });
+            xHero.$list.find('.hero-slide:first-child').css('display', 'block');
             xHero.$list.find('.last-hero').css('z-index', 0);
         },
         onResize: function() {
@@ -104,8 +106,9 @@ $(document).ready(function () {
             else
                 xHero.$overlay.on('mouseenter touchstart', xHero.pause).on('mouseleave touchend', xHero.start);
             xHero.$radio.on('click', function () {
+                xHero.previous = xHero.current;
                 xHero.current = this.value;
-                xHero.swap(null);
+                xHero.swap();
             });
             xHero.$window.on('resize', function () {
                 xHero.onResize();
@@ -113,45 +116,33 @@ $(document).ready(function () {
         },
         start: function () {
             xHero.interval = setInterval(function () {
-                xHero.swap(xHero.AUTO);
-            }, xHero.sleepTimer);
+                xHero.previous = xHero.current;
+                xHero.current++;
+                xHero.swap();
+            }, xHero.sleepTimer + xHero.animationSpeed);
         },
         pause: function () {
             clearInterval(xHero.interval);
         },
-        swap: function (type) {
+        swap: function () {
+            if(xHero.current == xHero.previous)
+                return;
             if (xHero.slideInFomSide) {
-                xHero.$list.animate({'margin-left': (type == xHero.AUTO) ?
-                '-=' + 100 + '%' : -(xHero.current * 100) + '%'}, xHero.animationSpeed, function() {
-                    if(type == xHero.AUTO)
-                        xHero.current++;
+                xHero.$list.animate({'margin-left': -(xHero.current * 100) + '%'}, xHero.animationSpeed, function() {
                     if (xHero.current == xHero.files.length) {
-                        xHero.$list.css('margin-left', 0 + '%');
                         xHero.current = 0;
+                        xHero.$list.css('margin-left', 0 + '%');
                     }
                     xHero.$overlay.find('#hero-button-' + xHero.current).prop('checked', true);
                 });
             } else {
-                if(type != xHero.AUTO) {
-                    $.each(xHero.$elements, function(index, slide) {
-                        if(Math.abs($(slide).css('z-index') - xHero.files.length) < xHero.current) {
-                            $(this).fadeOut(xHero.animationSpeed);
-                        } else {
-                            $(this).fadeIn(xHero.animationSpeed);
-                        }
-                    });
-                    return;
+                if(xHero.current == xHero.files.length) {
+                    xHero.current = 0;
                 }
-                xHero.$list.find('.hero-slide-' + xHero.current).fadeOut(xHero.animationSpeed).promise().done(function() {
-                    if(xHero.current == 0)
-                        xHero.$list.find('.last-hero').show();
-                    xHero.current++;
-                    if(xHero.current == xHero.files.length) {
-                        xHero.current = 0;
-                        xHero.$elements.show();
-                    }
+                xHero.$list.find('.hero-slide-' + xHero.current).fadeIn(xHero.animationSpeed, function () {
                     xHero.$overlay.find('#hero-button-' + xHero.current).prop('checked', true);
                 });
+                xHero.$list.find('.hero-slide-' + xHero.previous).fadeOut(xHero.animationSpeed);
             }
         }
     };
